a62db553ee7ac38a94a78d63d93166ee54fbb193
Ken <musyaffa560@gmail.com>
2022-09-14 16:12:45 +0700

    Task/11_Introduction React : Finishing all task

---
+++ 11_Introduction React/pratikum/pratikum_react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './task/Home';
+
+import "../src/asset/css/main.css";
+import "../src/asset/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
